9c0da7fbac2bc59834686f9050a670fc91b6f613
anagha <anagharaj95@gmail.com>
2020-02-03 16:11:58 +0530

    mongodb

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5800;
@@ -9,6 +10,23 @@ const port = process.env.PORT || 5800;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
+});
+
+const exerciseRouter = require("./routes/exercises");
+const userRouter = require("./routes/users");
+
+app.use("/exercises", exerciseRouter);
+app.use("/users", userRouter);
+
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
